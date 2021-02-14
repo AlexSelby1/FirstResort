25c05d104ac7da2b95528d1f5ae43b29cb75c0c0
AlexSelby1 <62037865+AlexSelby1@users.noreply.github.com>
2021-02-14 14:54:45 +0000

    Added header, footer and slideshow

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,20 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 8000); // Change image every 4 seconds
+}
 
 require("@rails/ujs").start()
 require("turbolinks").start()
@@ -15,3 +29,4 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
