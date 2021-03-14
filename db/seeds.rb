5f048af1b9683b0cba673fce2a00e4ee9df93c4a
AlexSelby1 <62037865+AlexSelby1@users.noreply.github.com>
2021-03-14 15:20:44 +0000

    Editing seeds for quick input

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.delete_all
+Category.create(:id => 1, :title=>'Bar', :image=>'barCat.jpeg')
+Category.create(:id => 2, :title=>'Floor', :image=>'wait.jpeg')
+Category.create(:id => 3, :title=>'Kitchen', :image=>'chef.jpeg')
+Category.create(:id => 4, :title=>'Cafe', :image=>'Cafe.jpeg')
+puts "Success: Category data loaded"
