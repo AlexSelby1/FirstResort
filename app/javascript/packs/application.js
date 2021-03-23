1181256477ab32f52a6b79b561259232e2600770
AlexSelby1 <alex.selby@hotmail.com>
2021-03-23 19:19:17 +0000

    Added JavaScript to Home

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,8 +2,6 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-//= require trix
-//= require_tree 
 
 require("@rails/ujs").start()
 require("turbolinks").start()
@@ -11,16 +9,12 @@ require("@rails/activestorage").start()
 require("channels")
 require("trix")
 require("@rails/actiontext")
+require("./homeinfo")
+require("./slideshow")
+  
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-
-function myFunction() {
-    document.getElementById("demo").innerHTML = "A Workawayer is: a traveller who wants to give back to the communities and places they visit. Open to helping hosts and using the experience to learn and immerse in the local culture.";
-  }
-  function uncheck() {
-    document.getElementById("demo").checked = false;
-  }
--- app/javascript/packs/slideshow.js
+++ app/javascript/packs/slideshow.js
@@ -1,10 +1,10 @@
-var slideIndex = 0;
-showSlides();
+console.log("Hello from slideshow.js");
 
-window.showSlides() = function() {
+
+window.showSlides = function() {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
+  for (var i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   slideIndex++;
@@ -12,3 +12,5 @@ window.showSlides() = function() {
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 8000); // Change image every 4 seconds
 }
+var slideIndex = 0;
+showSlides();
