11696fb2876aee0d12b69004145cca1100bba701
AlexSelby1 <alex.selby@hotmail.com>
2021-04-12 17:10:30 +0100

    Updating Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,17 @@ Category.create(:id => 3, :title=>'Kitchen', :image=>'chefCat.jpeg')
 Category.create(:id => 4, :title=>'Cafe', :image=>'cafeCat.jpeg')
 puts "Success: Category data loaded"
 
+User.delete_all
+User.create(:id => 1, :name=>'McCarthys Bar and Grill', :password=>'password', :email=>'host@test.com', :isHost=>'true', :isAdmin=>'false', isCandidate:'false',:country=>'IE')
+User.create(:id => 2, :name=>'Fattys Bar', :password=>'password', :email=>'host2@test.com', :isHost=>'true', :isAdmin=>'false', isCandidate:'false',:country=>'BE')
+User.create(:id => 3, :name=>'Smokys Cafe', :password=>'password', :email=>'host3@test.com', :isHost=>'true', :isAdmin=>'false', isCandidate:'false',:country=>'ES')
+User.create(:id => 4, :name=>'Jean Elise', :password=>'password', :email=>'candidate@test.com', :isHost=>'false', :isAdmin=>'false', isCandidate:'true',:country=>'FR')
+User.create(:id => 5, :name=>'Tom O Hara', :password=>'password', :email=>'candidate2@test.com', :isHost=>'false', :isAdmin=>'false', isCandidate:'true',:country=>'SC')
+User.create(:id => 6, :name=>'John Morahan', :password=>'password', :email=>'candidate3@test.com', :isHost=>'false', :isAdmin=>'false', isCandidate:'true',:country=>'IE')
+User.create(:id => 7, :name=>'Alex Admin', :password=>'password', :email=>'admin@test.com', :isHost=>'false', :isAdmin=>'true', isCandidate:'false',:country=>'IE')
+puts "Success: User data loaded"
+
+#Job.delete_all
+#Job.create(:id => 1, :title=>'Barman required for summer period', :country=>'Ireland', :category=>'Bar', :user_id=>'1')
+#Job.create(:id => 2, :title=>'Looking for a Chef - 5 years experience', :country=>'Ireland', :category=>'Kitchen', :user_id=>'1')
+#Job.create(:id => 3, :title=>'URGENT - Bar Manager role available', :country=>'Ireland', :category=>'Bar', :user_id=>'1')
